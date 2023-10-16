8970bebd2865ae35a23f957dcd44170baba579a1
Calsyd <72507262+Calsyd@users.noreply.github.com>
2023-10-16 16:11:01 +0200

    Enemy: chase, attack

--- Assets/Scripts/AI/EnemyAI.cs
+++ Assets/Scripts/AI/EnemyAI.cs
@@ -10,16 +10,21 @@ public class EnemyAI : MonoBehaviour
 
     [SerializeField] Points points;
     [SerializeField] Animator animator;
+    [SerializeField] Transform target;
+    [SerializeField] float minDistanceTargetChase;
+    [SerializeField] float minDistanceTargetAttack;
 
-    private PatrolState PatrolState { get; set; }
-    private AttackState AttackState { get; set; }
+    public PatrolState PatrolState { get; private set; }
+    public AttackState AttackState { get; private set; }
+    public ChaseState ChaseState { get; private set; }
 
     private void Awake()
     {
-        enemyData = new EnemyData(GetComponent<NavMeshAgent>(),animator);
+        enemyData = new EnemyData(GetComponent<NavMeshAgent>(),animator,target);
 
-        PatrolState = new PatrolState(this, enemyData, points.PointsObject);
-        AttackState = new AttackState(this, enemyData);
+        PatrolState = new PatrolState(this, enemyData, points.PointsObject, 6, minDistanceTargetChase);
+        AttackState = new AttackState(this, enemyData, minDistanceTargetAttack);
+        ChaseState = new ChaseState(this, enemyData, 8, minDistanceTargetAttack, minDistanceTargetChase);
 
         ChangeState(PatrolState);
     }
@@ -48,10 +53,12 @@ public class EnemyData
 {
     public NavMeshAgent Agent;
     public Animator AnimatorController;
+    public Transform Target;
 
-    public EnemyData(NavMeshAgent agent, Animator animatorController)
+    public EnemyData(NavMeshAgent agent, Animator animatorController, Transform target)
     {
         Agent = agent;
         AnimatorController = animatorController;
+        Target = target;
     }
 }
--- Assets/Scripts/AI/StateAI.cs
+++ Assets/Scripts/AI/StateAI.cs
@@ -7,14 +7,18 @@ public class PatrolState: IEnemyState
     private EnemyAI enemyAI;
     private EnemyData enemyData;
     private Transform[] points;
+    private float defaultSpeed;
+    private float minDistanceTarget;
 
     private int destPoint;
 
-    public PatrolState(EnemyAI ai, EnemyData enemyData, Transform[] points)
+    public PatrolState(EnemyAI ai, EnemyData enemyData, Transform[] points, float defaultSpeed, float minDistanceTarget)
     {
         enemyAI = ai;
         this.enemyData = enemyData;
         this.points = points;
+        this.defaultSpeed = defaultSpeed;
+        this.minDistanceTarget = minDistanceTarget;
     }
 
     public void Enter()
@@ -22,6 +26,7 @@ public class PatrolState: IEnemyState
         enemyData.Agent.autoBraking = false;
         GoToNextPoint();
         enemyData.AnimatorController.SetBool("IsWalking", true);
+        enemyData.Agent.speed = defaultSpeed;
     }
 
     public void Exit()
@@ -34,6 +39,11 @@ public class PatrolState: IEnemyState
         if (!enemyData.Agent.pathPending && enemyData.Agent.remainingDistance < 2f)
         {
             GoToNextPoint();
+        }
+        float distanceTarget = Vector3.Distance(enemyAI.transform.position, enemyData.Target.position);
+        if (distanceTarget < minDistanceTarget)
+        {
+            enemyAI.ChangeState(enemyAI.ChaseState);
         }
     }
     void GoToNextPoint()
@@ -53,25 +63,78 @@ public class AttackState : IEnemyState
 {
     private EnemyAI enemyAI;
     private EnemyData enemyData;
+    private float minDistanceTarget;
 
-    public AttackState(EnemyAI ai, EnemyData enemyData)
+    public AttackState(EnemyAI ai, EnemyData enemyData, float minDistanceTarget)
     {
         enemyAI = ai;
         this.enemyData = enemyData;
+        this.minDistanceTarget = minDistanceTarget;
     }
 
     public void Enter()
     {
-
+        enemyData.AnimatorController.SetBool("Attack", true);
     }
 
     public void Exit()
     {
+        enemyData.AnimatorController.SetBool("Attack", false);
+    }
+
+    public void Update()
+    {
+        float distance = Vector3.Distance(enemyAI.transform.position, enemyData.Target.position);
+        if (distance > minDistanceTarget)
+        {
+            enemyAI.ChangeState(enemyAI.ChaseState);
+        }
+    }
+}
+public class ChaseState : IEnemyState
+{
+    private EnemyAI enemyAI;
+    private EnemyData enemyData;
+    private float speedChase;
+    private float minDistanceTarget;
+    private float minDistanceTargetChase;
+
+
+    public ChaseState(EnemyAI ai, EnemyData enemyData, float speedChase, float minDistanceTarget, float minDistanceTargetChase)
+    {
+        enemyAI = ai;
+        this.enemyData = enemyData;
+        this.speedChase = speedChase;
+        this.minDistanceTarget = minDistanceTarget;
+        this.minDistanceTargetChase = minDistanceTargetChase;
+    }
+
+    public void Enter()
+    {
+        enemyData.AnimatorController.SetBool("IsRun", true);
+        enemyData.Agent.speed = speedChase;
+    }
+
+    public void Exit()
+    {
+        enemyData.AnimatorController.SetBool("IsRun", false);
 
     }
 
     public void Update()
     {
+        enemyData.Agent.SetDestination(enemyData.Target.position);
+
+        float distanceTarget = Vector3.Distance(enemyAI.transform.position, enemyData.Target.position);
+
+        if (distanceTarget < minDistanceTarget) //attack
+        {
+            enemyAI.ChangeState(enemyAI.AttackState);
+        }
+        if (distanceTarget > minDistanceTargetChase)
+        {
+            enemyAI.ChangeState(enemyAI.PatrolState);
+        }
 
     }
 }
